3839db9f555214255a0a37b7a74aa833923b31f1
villanoo <oscar.villanova@roche.com>
2020-10-22 12:41:53 +0200

    initial commit

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,16 @@
+declare namespace Cypress {
+    interface Resource {
+      path: string;
+      fixture?: string;
+      alias?: string;
+    }
+  
+    interface Chainable {
+      loadAndVisit(
+        visitUrl: string,
+        resources: Resource[],
+        callbackAfterVisit?: () => void
+      ): Chainable<Element>;
+    }
+  }
+  
